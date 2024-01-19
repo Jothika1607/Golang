53a550e54aea74e9bf1040cb119b88a47fbd1217
jothika-datasirpi <jothika.s@datasirpi.com>
2024-01-19 22:45:14 +0530

    go

---
+++ struct/struct.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// declare a struct
+	type Person struct {
+		name string
+		age  int
+	}
+
+	// assign value to struct while creating an instance
+	person1 := Person{"John", 25}
+	fmt.Println(person1)
+	fmt.Println("Person 1 nae :", person1.name)
+
+	// define an instance
+	var person2 Person
+
+	// assign value to struct variables
+	person2 = Person{
+		name: "Sara",
+		age:  29,
+	}
+
+	fmt.Println(person2)
+}
